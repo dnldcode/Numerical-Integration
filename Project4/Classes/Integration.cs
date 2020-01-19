e65c69bae7694f1fbbc2f3430fd825aac2cb9543
Donald <dnldcode@gmail.com>
2020-01-19 02:36:38 +0100

    first commit

---
+++ Project4/Classes/Integration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4.Classes
+{
+	class Integration
+	{
+		Polynomial _polynomial;
+
+		public Integration(Polynomial polynomial)
+		{
+			_polynomial = polynomial;
+		}
+
+		public double GetIntegralTrapeze(double a, double b, int n = 1000)
+		{
+			double h = (b - a) / n;
+			double I = h / 2 * (_polynomial.Calculate(a) + _polynomial.Calculate(b));
+			for (int i = 1; i < n; i++)
+				I += h * _polynomial.Calculate(a + i * h);
+
+			return I;
+		}
+
+		public double GetIntegralSimpson(double a, double b, int n = 2)
+		{
+			if (n % 2 != 0) throw new Exception("Value n should be even!");
+
+			double h = (b - a) / n;
+			double I = h / 3 * (_polynomial.Calculate(a) + _polynomial.Calculate(b));
+			for (int i = 1; i < n; i++)
+			{
+				int coefficient = (i / 2 == 0) ? 2 : 4;
+				I += coefficient * _polynomial.Calculate(a + i * h);
+			}
+
+			return I;
+		}
+	}
+}
---
+++ Project4/Program.cs
@@ -0,0 +1,37 @@
+﻿using Project4.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("Please write the coefficients of polynomial function:");
+				String input = Console.ReadLine();
+				Polynomial polynomial = new Polynomial(input);
+				Console.WriteLine("The function you've entered:");
+				Console.WriteLine(polynomial);
+				Console.WriteLine("Please write the boundaries of integration:");
+				Console.Write("a = ");
+				double a = Int32.Parse(Console.ReadLine());
+				Console.Write("b = ");
+				double b = Int32.Parse(Console.ReadLine());
+				Integration integration = new Integration(polynomial);
+				Console.WriteLine("Ingeration by trapezoidal method: " + integration.GetIntegralTrapeze(a, b));
+				Console.WriteLine("Ingeration by Simpson's method: " + integration.GetIntegralSimpson(a, b));
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Error occured: " + e.Message);
+			}
+		}
+	}
+}
